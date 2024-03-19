49bde690f44b778840b403c3509ec7b008f26986
dattali18 <74492594+dattali18@users.noreply.github.com>
2024-03-19 14:34:04 +0200

    removed main function from testing

--- Tests/movies_api_tests.py
+++ Tests/movies_api_tests.py
@@ -5,9 +5,3 @@ movies_model = MoviesModel()
 def test_get_movies():
     movies: list[Movie] = movies_model.get_movies()
     print(movies)
-
-def main():
-    test_get_movies()
-
-if __name__ == "__main__":
-    main()
