e3b802f720f27c87cb18c468258796f29848be02
dattali18 <danielattali16@gmail.com>
2024-03-23 20:05:04 +0200

    added model to main window and added events

--- Views/main_window.py
+++ Views/main_window.py
@@ -1,11 +1,20 @@
 from PySide6 import QtWidgets
-from PySide6.QtWidgets import QPushButton, QTableWidget, QTableWidgetItem, QHeaderView
+from PySide6.QtWidgets import (
+    QPushButton,
+    QTableWidget,
+    QTableWidgetItem,
+    QHeaderView,
+    QLineEdit,
+)
 from PySide6.QtGui import QAction
 from PySide6.QtCore import QFile, QIODevice
 from PySide6.QtUiTools import QUiLoader
+
 import sys
+from typing import Optional
 
 from .form_ui import Ui_MainWindow
+from Models import *
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -16,9 +25,12 @@ class MainWindow(QtWidgets.QMainWindow):
 
 
 class Controller:
-    def __init__(self, view: MainWindow):
+    def __init__(self, view: MainWindow, model: LibrariesModel):
         self.view = view.ui
         self.window = view
+        self.model = model
+
+        self.search_field: QLineEdit = self.view.search_field
 
         self.search_btn: QPushButton = self.view.search_btn
         self.create_btn: QPushButton = self.view.create_btn
@@ -37,12 +49,22 @@ class Controller:
         self.update_btn.clicked.connect(self.handle_update)
         self.update_action.triggered.connect(self.handle_update)
 
-        # self.view.libraries_table: QTableWidget = self.view.libraries_view.libraries_table
+        self.view.libraries_table.setEditTriggers(
+            QTableWidget.EditTrigger.NoEditTriggers
+        )
 
+        self.view.libraries_table.cellClicked.connect(self.handle_click)
+
+        self.libraries: Optional[list[GetLibraryDto]] = self.model.get_libraries()
         self.populate_libraries_table()
 
     def handle_search(self):
-        print("search")
+        search_term: str = self.search_field.text()
+        if search_term == "":
+            self.libraries = self.model.get_libraries()
+        else:
+            self.libraries = self.model.get_libraries_name(name=search_term)
+        self.populate_libraries_table()
 
     def handle_create(self):
         print("create")
@@ -50,23 +72,40 @@ class Controller:
     def handle_update(self):
         print("update")
 
+    def handle_click(self, row: int, column: int):
+        print(f"item clicked at {row=}, {column=}")
+
     def populate_libraries_table(self):
+        # deleting all the old entries
+        self.view.libraries_table.setRowCount(0)
+
+        if self.libraries is None:
+            print("404 error please check backend")
+            return
+
         self.view.libraries_table.setColumnCount(3)
-        self.view.libraries_table.setRowCount(11)
+        self.view.libraries_table.setRowCount(len(self.libraries))
 
-        self.view.libraries_table.setItem(0, 0, QTableWidgetItem("Header 1"))
-        self.view.libraries_table.setItem(0, 1, QTableWidgetItem("Header 2"))
-        self.view.libraries_table.setItem(0, 2, QTableWidgetItem("Header 3"))
+        self.view.libraries_table.setHorizontalHeaderItem(0, QTableWidgetItem("Title"))
+        self.view.libraries_table.setHorizontalHeaderItem(
+            1, QTableWidgetItem("Keywords")
+        )
+        self.view.libraries_table.setHorizontalHeaderItem(2, QTableWidgetItem("Media"))
 
-        for i in range(3):
-            for j in range(10):
-                self.view.libraries_table.setItem(j + 1, i, QTableWidgetItem("Some text"))
+        for i, library in enumerate(self.libraries):
+            self.view.libraries_table.setItem(i, 0, QTableWidgetItem(library.name))
+            self.view.libraries_table.setItem(
+                i, 1, QTableWidgetItem(", ".join(library.keywords))
+            )
+            self.view.libraries_table.setItem(
+                i, 2, QTableWidgetItem(f"{len(library.media)}")
+            )
 
         self.view.libraries_table.horizontalHeader().setStretchLastSection(True)
         self.view.libraries_table.horizontalHeader().setSectionResizeMode(
             QHeaderView.Stretch
         )
-        self.view.libraries_table.verticalHeader().setStretchLastSection(True)
-        self.view.libraries_table.verticalHeader().setSectionResizeMode(
-            QHeaderView.Stretch
-        )
+        # self.view.libraries_table.verticalHeader().setStretchLastSection(True)
+        # self.view.libraries_table.verticalHeader().setSectionResizeMode(
+        #     QHeaderView.Stretch
+        # )
--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import sys
 from PySide6.QtWidgets import QApplication
 
 from Views import MainWindow, Controller
+from Models import LibrariesModel
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
-    controller = Controller(view=window)
+    controller = Controller(view=window, model=LibrariesModel())
     controller.window.show()
     sys.exit(app.exec())
