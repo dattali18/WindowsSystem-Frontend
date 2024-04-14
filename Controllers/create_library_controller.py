327203c0cdaa982aa6fc0e9e52c6982e91e76ae7
dattali18 <74492594+dattali18@users.noreply.github.com>
2024-04-14 18:31:04 +0300

    fixed formating in file

--- Controllers/create_library_controller.py
+++ Controllers/create_library_controller.py
@@ -1,6 +1,7 @@
 from Views import CreateLibraryView
 from Models import LibrariesModel
 
+
 class CreateLibraryController:
     def __init__(self, view: CreateLibraryView, model: LibrariesModel) -> None:
         self.view = view
